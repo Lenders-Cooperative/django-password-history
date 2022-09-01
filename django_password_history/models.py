549c8e91ba3c20e9a2b2518ef9e9753e710c975f
David Graves <dgraves@thesummitgrp.com>
2022-09-01 14:36:33 -0500

    expand history support for 24 passwords

--- django_password_history/models.py
+++ django_password_history/models.py
@@ -9,6 +9,10 @@ from django.conf import settings
 from django.apps import apps
 from django.contrib.auth.hashers import make_password, check_password
 
+DEFAULT_PASSWORD_COUNT = 5   # set to 5 to maintain backward compatibility
+
+
+# Note: A future version of this will likely change to use an ArrayField or JSONField
 
 class UserPasswordHistory(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -17,26 +21,45 @@ class UserPasswordHistory(models.Model):
     password_3 = models.CharField(blank=True, null=True, max_length=128)
     password_4 = models.CharField(blank=True, null=True, max_length=128)
     password_5 = models.CharField(blank=True, null=True, max_length=128)
+    password_6 = models.CharField(blank=True, null=True, max_length=128)
+    password_7 = models.CharField(blank=True, null=True, max_length=128)
+    password_8 = models.CharField(blank=True, null=True, max_length=128)
+    password_9 = models.CharField(blank=True, null=True, max_length=128)
+    password_10 = models.CharField(blank=True, null=True, max_length=128)
+    password_11 = models.CharField(blank=True, null=True, max_length=128)
+    password_12 = models.CharField(blank=True, null=True, max_length=128)
+    password_13 = models.CharField(blank=True, null=True, max_length=128)
+    password_14 = models.CharField(blank=True, null=True, max_length=128)
+    password_15 = models.CharField(blank=True, null=True, max_length=128)
+    password_16 = models.CharField(blank=True, null=True, max_length=128)
+    password_17 = models.CharField(blank=True, null=True, max_length=128)
+    password_18 = models.CharField(blank=True, null=True, max_length=128)
+    password_19 = models.CharField(blank=True, null=True, max_length=128)
+    password_20 = models.CharField(blank=True, null=True, max_length=128)
+    password_21 = models.CharField(blank=True, null=True, max_length=128)
+    password_22 = models.CharField(blank=True, null=True, max_length=128)
+    password_23 = models.CharField(blank=True, null=True, max_length=128)
+    password_24 = models.CharField(blank=True, null=True, max_length=128)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.username + "_password_history"
+        return self.user.username + '_password_history'
 
     def password_is_used(self, password, site_id=1):
-        previous_passwords_count = 5
+        previous_passwords_count = DEFAULT_PASSWORD_COUNT
         SiteSettings = None
 
-        use_site_setting_password_history = getattr(settings,"USE_SITE_SETTINGS_PASSWORD_HISTORY",False)
+        use_site_setting_password_history = getattr(settings, 'USE_SITE_SETTINGS_PASSWORD_HISTORY', False)
         
         try:
-            SiteSettings = apps.get_model("setup","SiteSettings")
+            SiteSettings = apps.get_model('setup', 'SiteSettings')
         except:
             SiteSettings = None
 
         if use_site_setting_password_history and SiteSettings:
             previous_passwords_count = SiteSettings.objects.get(id=site_id).previous_password_count
         else:
-            previous_passwords_count = getattr(settings,"PREVIOUS_PASSWORD_COUNT", 5)
+            previous_passwords_count = getattr(settings, 'PREVIOUS_PASSWORD_COUNT', DEFAULT_PASSWORD_COUNT)
 
         if previous_passwords_count:
             for x in range(1, min(previous_passwords_count, 5) + 1):
@@ -47,9 +70,9 @@ class UserPasswordHistory(models.Model):
         return False
 
     def store_password(self):
-        self.password_5 = self.password_4
-        self.password_4 = self.password_3
-        self.password_3 = self.password_2
-        self.password_2 = self.password_1
+        for x in range(2, 25)[::-1]:
+            """ Shift all the existing passwords """
+            setattr(self, f'password_{x}', getattr(self, f'password_{x-1}'))
+
         self.password_1 = self.user.password
         self.save()
