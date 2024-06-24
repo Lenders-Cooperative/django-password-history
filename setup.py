33c6a71728abe6651da89790e4157acf6decc6e1
Logan Bibby <lbibby@thesummitgrp.com>
2024-06-24 09:12:24 -0500

    Update requirements; version bump

--- setup.py
+++ setup.py
@@ -59,8 +59,8 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'django-model-utils==4.2.0',
-        'djangoql==0.17.1',
+        'django-model-utils>=4.2.0',
+        'djangoql>=0.17.1',
     ],
     license="BSD",
     zip_safe=False,
